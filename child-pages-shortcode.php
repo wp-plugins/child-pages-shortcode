c90107a192ed18601bb38eaa78969714371b1e66
miyauchi <miyauchi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-20 19:09:36 +0000

    ver 0.5.0
    git-svn-id: https://plugins.svn.wordpress.org/child-pages-shortcode/trunk@465348 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- child-pages-shortcode.php
+++ child-pages-shortcode.php
@@ -4,7 +4,7 @@ Plugin Name: Child Pages Shortcode
 Author: Takayuki Miyauchi
 Plugin URI: http://wpist.me/wp/child-pages-shortcode/
 Description: You can use shortcode for display child pages from the page.
-Version: 0.4.0
+Version: 0.5.0
 Author URI: http://wpist.me/
 Domain Path: /languages
 Text Domain: child-pages-shortcode
@@ -69,7 +69,7 @@ private function display($p)
         'post_parent' => $p['id'],
         'orderby' => 'menu_order',
         'order' => 'ASC',
-        'numberposts' => '-1',
+        'nopaging' => true,
     );
 
     query_posts($args);
